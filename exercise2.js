3626e610edcfbebf0442d82323853dd234340e18
Jonathan Leung <j0nathanleungs@gmail.com>
2019-07-28 13:06:08 -0700

    first commit

---
+++ exercise2.js
@@ -0,0 +1,7 @@
+var name = prompt("Enter your name: ");
+var age = prompt("Enter your age in the year 2017: ");
+ document.getElementById("p1").innerHTML= name + ' ' + (2017 - age);
+ var num1 = +prompt("Enter a number:");
+ var num2 = +prompt("Enter a number:");
+ var total = Math.pow(num1,3) + Math.pow(num2,3);
+ alert("the sum of these numbers cubed: "+ total);
